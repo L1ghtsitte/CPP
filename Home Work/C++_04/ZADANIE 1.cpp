cc739e7adde876912b2c98bec73281618da02568
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-01 09:52:26 +0300

    Update ZADANIE 1.cpp

--- Home Work/C++_04/ZADANIE 1.cpp
+++ Home Work/C++_04/ZADANIE 1.cpp
@@ -10,16 +10,18 @@ int main()
 	double R2 = 0;
 	double R3 = 0;
 
-	std::cout << "Âåäèòå çíà÷åíèå R1 = ";
+	std::cout << "Ã‚Ã¥Ã¤Ã¨Ã²Ã¥ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ R1 = ";
 	std::cin >> R1;
-	std::cout << "Âåäèòå çíà÷åíèå R2 = ";
+	std::cout << "Ã‚Ã¥Ã¤Ã¨Ã²Ã¥ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ R2 = ";
 	std::cin >> R2;
-	std::cout << "Âåäèòå çíà÷åíèå R3 = ";
+	std::cout << "Ã‚Ã¥Ã¤Ã¨Ã²Ã¥ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ R3 = ";
 	std::cin >> R3;
 
 	R0 = 1 / R1 + 1 / R2 + 1 / R3;
 
 	R0 = 1 / R0;
 
-	std::cout << "Çíà÷åíèå R0 = " << R0;
+	std::cout << "Ã‡Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ R0 = " << R0;
+
+	//Ğ’Ñ‹Ğ¿Ğ¾Ğ»Ğ½Ğ¸Ğ» L1ghtsitte//https://github.com/L1ghtsitte/CPP
 }
