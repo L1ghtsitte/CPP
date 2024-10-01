6f117f09c8576a208ba15d53e5e82209f0f888b6
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-01 23:18:44 +0300

    Add files via upload

---
+++ game_first/gameOver/GameOver 3.0.cpp
@@ -0,0 +1,14 @@
+//Программа GameOver 3.0;
+//Демонстрирует работу с объявлениеми using;
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main ()
+{
+	cout << "Game Over!";
+
+	return 0;
+}
---
+++ game_first/gameStats/GameStats 2.0.cpp
@@ -0,0 +1,13 @@
+//Программа Game Stats 2.0;
+//Демонстрирует арифметические операции переменными;
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned int score = 5000;
+	cout << " Score = " << score << endl;
+
+}
