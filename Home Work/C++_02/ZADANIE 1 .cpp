086d0561ce906862a2bd05cc415ebcd0481a9a8d
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-01 09:53:15 +0300

    Update ZADANIE 1 .cpp

--- Home Work/C++_02/ZADANIE 1 .cpp
+++ Home Work/C++_02/ZADANIE 1 .cpp
@@ -7,4 +7,6 @@ int main()
 	cout << "\n\n Rammstein - Amerika \n\n We're all living in Amerika \n Amerika ist wunderbarWe're all living in Amerika Amerika, Amerika \n "
 		 <<  "We're all living in Amerika \n Amerika ist wunderbar \n We're all living in Amerika\n Amerika, Amerika \n\n Rammstein - Amerika ";
 	return 0;
+
+	//Выполнил L1ghtsitte//https://github.com/L1ghtsitte/CPP
 }
