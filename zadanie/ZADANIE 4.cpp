1f59b87e3b597eb5e227d06e0133370f65da0256
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-01 09:56:26 +0300

    Update ZADANIE 4.cpp

--- zadanie/ZADANIE 4.cpp
+++ zadanie/ZADANIE 4.cpp
@@ -16,4 +16,6 @@ int main()
 	std::cout << "Разница: " << num1 - num2 << "\n";
 	std::cout << "Сумма: " << num1 + num2 << "\n";
 	std::cout << "Произведение: " << num1 * num2 << "\n";
+
+	//Выполнил L1ghtsitte//https://github.com/L1ghtsitte/CPP
 }
