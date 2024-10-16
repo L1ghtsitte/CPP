df89c3c47efd8d16d9f66910f8ce5d47ecd5e139
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-17 01:48:15 +0300

    Add files via upload

---
+++ lessons/lesson 1/lesson 1.cpp
@@ -0,0 +1,17 @@
+	/*#include <iostream>*/ // 1 \\
+
+/*								
+int main()	///2
+{								
+	int num;					
+								
+	num = 10;					
+								
+	num = -num;					
+								
+	std::cout << num; // 3 \\	
+								
+	return 0; // 4 \\			
+								
+}	2\\\							
+*/ 
