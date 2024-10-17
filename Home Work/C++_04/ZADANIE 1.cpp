f5df8f541ce20e36fca72ff5166eb5df1858d5b4
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-18 00:48:56 +0300

    Update ZADANIE 1.cpp

--- Home Work/C++_04/ZADANIE 1.cpp
+++ Home Work/C++_04/ZADANIE 1.cpp
@@ -10,18 +10,19 @@ int main()
 	double R2 = 0;
 	double R3 = 0;
 
-	std::cout << "Âåäèòå çíà÷åíèå R1 = ";
+	std::cout << "Введите значение  R1 = ";
 	std::cin >> R1;
-	std::cout << "Âåäèòå çíà÷åíèå R2 = ";
+	std::cout << "Введите значение  R2 = ";
 	std::cin >> R2;
-	std::cout << "Âåäèòå çíà÷åíèå R3 = ";
+	std::cout << "Введите значение  R3 = ";
 	std::cin >> R3;
 
 	R0 = 1 / R1 + 1 / R2 + 1 / R3;
 
 	R0 = 1 / R0;
 
-	std::cout << "Çíà÷åíèå R0 = " << R0;
+	std::cout << "Значение R0 = " << R0;
 
-	//Выполнил L1ghtsitte//https://github.com/L1ghtsitte/CPP
+//whrite by L1ghtsitte\\
+//{https://github.com/L1ghtsitte/CPP}\\
 }
