dadd93f618833585719de4c20c9ec1fe53cf6608
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-18 00:45:58 +0300

    Update ZADANIE 2.cpp

--- Home Work/C++_02/ZADANIE 2.cpp
+++ Home Work/C++_02/ZADANIE 2.cpp
@@ -13,5 +13,6 @@ int main()
 	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
 	return 0;
 
-	//Выполнил L1ghtsitte//https://github.com/L1ghtsitte/CPP
+//whrite by L1ghtsitte\\
+//{https://github.com/L1ghtsitte/CPP}\\
 }
