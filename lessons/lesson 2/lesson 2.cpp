6b89b750358334973e6778da60c3cacf6fbb54e4
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-17 18:40:42 +0300

    Add files via upload

---
+++ lessons/lesson 2/lesson 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+
+	char var1 = 'A';   // 1 выделяет в битах
+	bool var2 = false;  // 1 выделяет в битах
+	int var3 = 8;	   // 4 выделяет в битах
+	float var4 = 12.3; // 4 выделяет в битах
+	double var5 = 5.3; // 8 выделяет в битах
+
+}
