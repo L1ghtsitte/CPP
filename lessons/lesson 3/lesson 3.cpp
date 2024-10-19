7f33700bcaf00eb0ea2969b713d40a3b2fc92ea0
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-10-20 00:40:20 +0300

    Update lesson 3.cpp

--- lessons/lesson 3/lesson 3.cpp
+++ lessons/lesson 3/lesson 3.cpp
@@ -1,1 +1,34 @@
-1
+#include <iostream>
+
+int main()
+{
+
+	int num;
+
+	while (true)
+	{
+
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << i << ". Hello world!" << std::endl;
+		}
+
+		std::cin >> num;
+
+		if (num < 0)
+		{
+			std::cout << "negativ" << std::endl;
+		}
+		else
+		{
+			std::cout << "pozitiv" << std::endl;
+		}
+
+		system("pause");
+		system("cls");
+
+	}
+}
+
+//whrite by L1ghtsitte\\
+//{https://github.com/L1ghtsitte/CPP}\\
